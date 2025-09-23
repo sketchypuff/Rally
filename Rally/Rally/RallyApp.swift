8cbe8f344f5f57c77e5fe389e336b5bb090019f2
Yash Shenai <yashrox1999@gmail.com>
2025-09-23 19:57:54 +0530

    Initialize project

---
+++ Rally/Rally/RallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RallyApp.swift
+//  Rally
+//
+//  Created by Yash Shenai on 23/09/25.
+//
+
+import SwiftUI
+
+@main
+struct RallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Rally/RallyTests/RallyTests.swift
@@ -0,0 +1,17 @@
+//
+//  RallyTests.swift
+//  RallyTests
+//
+//  Created by Yash Shenai on 23/09/25.
+//
+
+import Testing
+@testable import Rally
+
+struct RallyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
